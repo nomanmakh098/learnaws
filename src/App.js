697a0fe1ba4a41180257a536a3cac6e27d7d7855
Makhdoom <nomanmakh@yahoo.com>
2019-10-25 16:31:06 -0400

    ddd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload......opopo
+          Edit <code>src/App.js</code> and save to reload......opopofffff
         </p>
         <a
           className="App-link"
